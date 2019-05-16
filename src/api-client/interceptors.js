b20d7bef8981b8acfd0c96f9b32793019221b619
Dima True <montifick77@gmail.com>
2019-05-16 13:05:29 +0300

    refactoring Loading

--- src/api-client/interceptors.js
+++ src/api-client/interceptors.js
@@ -6,12 +6,19 @@ export default {
     axios.interceptors.response.use(function (response) {
       return response;
     }, function (error) {
+      if (error.response.status === 400) {
+        alert('Sorry, Check your data and try again')
+      }
       if (error.response.status === 401) {
         store.dispatch(logOut());
         history.push('/login');
       }
       if (error.response.status === 404) {
         history.push('/error_404_');
+      }
+      if (error.response.status === 500) {
+        alert('Keep calm and try again later ;P')
+        history.push('/login');
       }
       return Promise.reject(error);
     })
--- src/components/CommonComponents/Loading/Loading.js
+++ src/components/CommonComponents/Loading/Loading.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
 import style from './Loading.module.scss';
 
 
 const Loading = () => (
+  <>
+  <Header />
   <div className={style.cont}>
     <h1 className={style.heading}> Loading. . . </h1>
     <div className={style.loader}></div>
   </div>
+  <Footer />
+  </>
 );
 
 export default Loading;
